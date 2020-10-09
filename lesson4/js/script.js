74ca6300025c8c4f6136afa08f08261e9dbe90bd
JaxsonJohnson <Jaxson@byui.edu>
2020-10-09 13:31:12 -0600

    initial commit

---
+++ lesson4/js/script.js
@@ -0,0 +1,6 @@
+let lastmodif = document.lastModified;
+document.getElementById('lastmodif').textContent = lastmodif;
+
+function toggleMenu(){
+    document.getElementById('primaryNav').classList.toggle('hide')
+}
